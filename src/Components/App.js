90826edcae6d88398727d77391a21429f2c73296
jinyoung1005 <himyfriend17@naver.com>
2020-12-09 20:29:01 +0900

    안녕

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Component} from 'react';
+import Router from '../Components/Router';
+
+class App extends Component {
+  render() {
+    return (
+    <>
+      <Router />
+    </>
+    );
+  }
+}
+
+
+export default App;
